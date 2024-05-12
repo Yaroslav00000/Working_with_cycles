30dce8a01a8feeef1d32aa3181c054df6d8685f6
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-12 22:19:37 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -5,7 +5,22 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
+    int low, up, number;
+    int sum = 0;
+    cout << "Введіть нижню межу: ";
+    cin >> low;
+    cout << "Введіть верхню межу: ";
+    cin >> up;
+    if (low > up) {
+        cout << "Неправильні межі. Нижня межа не може бути більшою за верхню." << endl;
+        return 1;
+    }
+    number = low;
+    while (number <= up) {
+        sum += number;
+        number++;
+    }
+    cout << "Сума чисел: " << sum << endl;
     system("pause");
     return 0;
 }
