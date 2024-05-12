6ee61643a6373afba2fdb2020c281d503e13475f
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-12 22:43:33 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <Windows.h>
+#include <cmath>
+
 using namespace std;
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
+    double x, y, result;
+    cout << "¬вед≥ть число x: ";
+    cin >> x;
+    cout << "¬вед≥ть степ≥нь y: ";
+    cin >> y;
+    result = pow(x, y);
+    cout << x << " у степен≥ " << y << " дор≥внюЇ " << result << endl;
     system("pause");
     return 0;
 }
