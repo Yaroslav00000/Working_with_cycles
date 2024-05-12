7c03648b1471e9da7e3a4daf37eab2d800a5f0a6
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-12 23:24:15 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
+    int a,number;
+    cout << "Введіть ціле число a (1 <= a <= 20): ";
+    cin >> a;
+    number = a;
+    if (a < 1 || a > 20) {
+        cout << "Неправильне значення a." << endl;
+    }
+    long long product = 1;
+    while (a <= 20) {
+        product *= a;
+        a++;
+    }
+    cout << "Добуток чисел від " << number << " до 20 дорівнює: " << product << endl;
     system("pause");
     return 0;
 }
