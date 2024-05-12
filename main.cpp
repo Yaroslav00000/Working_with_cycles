93f5b32926f7dcb69cdce250b344b68306d6cb5e
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-12 22:57:55 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -5,7 +5,17 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    double sum = 0;
+    int count = 0;
+    int number = 1;
+    while (number <= 1000) {
+        sum += number;
+        count++;
+        number++;
+    }
+    double average = sum / count; 
 
+    cout << "Середнє арифметичне від 1 до 1000: " << average << endl;
     system("pause");
     return 0;
 }
