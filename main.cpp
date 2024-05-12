bff13846cf163d9ebda9ee027bd448c8b9008ab2
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-12 23:30:12 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -5,7 +5,18 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
+    int k;
+    int i = 1;
+    cout << "Введіть номер варіанта (1-10): ";
+    cin >> k;
+    if (k < 1 || k > 10) {
+        cout << "Неправильний номер варіанта. Номер має бути в діапазоні від 1 до 10." << endl;
+    }
+    cout << "Таблиця множення на " << k << ":" << endl;
+    while (i <= 10) {
+        cout << k << " x " << i << " = " << k * i << endl;
+        i++;
+    }
     system("pause");
     return 0;
 }
