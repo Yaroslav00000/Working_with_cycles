98d8119c4404fd1d7af399f081541f4bfbcd1a1b
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-12 22:15:25 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -5,7 +5,55 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    int min, max;
+    cout << "Введіть мінімальне значення: ";
+    cin >> min;
+    cout << "Введіть максимальне значення: ";
+    cin >> max;
 
-    system("pause");
+
+    if (min > max) {
+        cout << "Мінімальне значення не може бути більшим за максимальне." << endl;
+    }
+    else{
+        int i = min;
+        cout << "Всі числа: ";
+        while (i <= max) {
+            cout << i << " ";
+            i++;
+        }
+        cout << endl;
+
+        i = min;
+        cout << "Парні числа: ";
+        while (i <= max) {
+            if (i % 2 == 0) {
+                cout << i << " ";
+            }
+            i++;
+        }
+        cout << endl;
+
+        i = min;
+        cout << "Непарні: ";
+        while (i <= max) {
+            if (i % 2 != 0) {
+                cout << i << " ";
+            }
+            i += 2;
+        }
+        cout << endl;
+
+        i = min;
+        cout << "Кратні 7: ";
+        while (i <= max) {
+            if (i % 7 == 0) {
+                cout << i << " ";
+            }
+            i++; 
+        }
+        cout << endl;
+    }
+    
     return 0;
 }
